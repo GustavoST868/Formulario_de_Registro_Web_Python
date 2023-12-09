17bdcc1112f0ed70ad3b2f5adf997a12c0b031fd
Gustavo Santos Teixeira <gustavosantosteixeira98@gmail.com>
2023-12-09 18:41:58 -0300

    .

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # Obtem os dados do formulário
+        nome = request.form['nome']
+        email = request.form['email']
+        cpf = request.form['cpf']
+        telefone = request.form['telefone']
+        data = request.form['data']
+        quantidade_pessoas = request.form['quantidade_pessoas']
+        
+        string = f'''
+        nome       = {nome}
+        email      = {email}
+        cpf        = {cpf}
+        telefone   = {telefone}
+        data       = {data}
+        quantidade = {quantidade_pessoas}
+        '''
+        print(string)
+
+    
+
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
